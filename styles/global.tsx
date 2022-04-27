9e1d1f99549b21366c4810ed813b6b658a4c7516
Marcus-Rise <ilykonst95@gmail.com>
2022-04-27 22:53:07 +0400

    feat(layout): header

--- styles/global.tsx
+++ styles/global.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
   html,
   body {
-    padding: 0;
+    padding: 0 !important;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
