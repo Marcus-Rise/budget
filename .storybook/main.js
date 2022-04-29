51b2f784e85232ffc6fcd29bb1e6661262bbb7cf
Marcus-Rise <ilykonst95@gmail.com>
2022-04-29 22:51:52 +0400

    feat: storybook webpack5 builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,21 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-next",
   ],
   framework: "@storybook/react",
   managerHead: (head, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      return (`
+    if (configType === "PRODUCTION") {
+      return `
         ${head}
         <base href="storybook/">
-      `);
+      `;
     }
   },
-}
+  core: {
+    builder: "webpack5",
+  },
+};
