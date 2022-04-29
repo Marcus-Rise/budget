d61343ae84e7951f211b8a00de28978c3f33408e
Marcus-Rise <ilykonst95@gmail.com>
2022-04-29 23:13:03 +0400

    docs: home page story

---
+++ src/stories/pages/home.stories.tsx
@@ -0,0 +1,43 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import Home from "../../../pages/index";
+import { useEffect, useState } from "react";
+import { TRANSACTION_LOCAL_STORAGE_KEY } from "../../transaction/transaction.hook";
+import { TRANSACTION_DATA_MOCK } from "../../transaction/transactions-data.mock";
+
+const Config: ComponentMeta<typeof Home> = {
+  title: "pages/Home",
+  component: Home,
+};
+
+const Default: ComponentStory<typeof Home> = (args) => {
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!isInitialized) {
+      localStorage.removeItem(TRANSACTION_LOCAL_STORAGE_KEY);
+      setIsInitialized(true);
+    }
+  }, [isInitialized]);
+
+  if (!isInitialized) {
+    return <></>;
+  }
+
+  return <Home {...args} />;
+};
+
+const WithData: ComponentStory<typeof Home> = (args) => {
+  const [isDataExists, setIsDataExists] = useState(false);
+
+  useEffect(() => {
+    if (!isDataExists) {
+      localStorage.setItem(TRANSACTION_LOCAL_STORAGE_KEY, JSON.stringify(TRANSACTION_DATA_MOCK));
+      setIsDataExists(true);
+    }
+  }, [isDataExists]);
+
+  return isDataExists ? <Home {...args} /> : <></>;
+};
+
+export default Config;
+export { Default, WithData };
