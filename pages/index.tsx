405fee6b444a44746ec0b678eb7793169551cbf1
Marcus-Rise <ilykonst95@gmail.com>
2022-05-13 13:32:25 +0400

    feat(transaction): default filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import { Container } from "../src/components/container";
+import type { TransactionFilter } from "../src/transaction/transaction.hook";
 import { useTransaction } from "../src/transaction/transaction.hook";
 import { Layout } from "../src/components/layout";
 import { TransactionStatistic } from "../src/transaction/components/statistic";
 import { TransactionList } from "../src/transaction/components/list";
 import { TransactionForm } from "../src/transaction/components/form";
 import { Button } from "../src/components/button";
+import type { TransactionModel } from "../src/transaction/models";
 import { TRANSACTION_CATEGORY_OTHER } from "../src/transaction/models";
 import styled from "styled-components";
 import { useState } from "react";
+import { isSameMonth } from "date-fns";
 
 const FormSubmitButton = styled(Button).attrs(() => ({
   type: "submit",
@@ -16,8 +19,15 @@ const FormSubmitButton = styled(Button).attrs(() => ({
   width: 100%;
 `;
 
+const isTransactionInSameMonth = (transaction: TransactionModel): boolean => {
+  return isSameMonth(transaction.date, new Date());
+};
+
 const Home: NextPage = () => {
-  const { saveTransaction, transactions, deleteTransaction } = useTransaction();
+  const [transactionFilters, setTransactionFilters] = useState<Array<TransactionFilter>>([
+    isTransactionInSameMonth,
+  ]);
+  const { saveTransaction, transactions, deleteTransaction } = useTransaction(transactionFilters);
   const [statisticFullView, setStatisticFullView] = useState(false);
   const toggleStatisticFullView = () => setStatisticFullView((full) => !full);
 
