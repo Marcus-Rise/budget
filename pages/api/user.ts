f1857057343148ecbf363180f005643cb733099b
Marcus-Rise <ilykonst95@gmail.com>
2022-05-19 22:57:17 +0400

    feat(user): get user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,16 +1,39 @@
 import type { NextApiHandler } from "next";
 import type { IUser } from "../../src/user";
 
-const UserHandler: NextApiHandler<IUser | { error: string }> = async (req, res) => {
-  const { auth } = req.cookies;
+const UserHandler: NextApiHandler<IUser | { error: string }> = async (req, response) => {
+  if (req.method !== "GET") {
+    response.status(405).end();
+  } else {
+    const { auth: token } = req.cookies;
 
-  if (!auth) {
-    return res.status(401).json({ error: "Unauthorized" });
+    if (!token) {
+      return response.status(401).json({ error: "Unauthorized" });
+    }
+
+    const user: { login: string } = await fetch(process.env.API_URL + "/api/user", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(async (res) => {
+        const json = await res.json();
+
+        if (!res.ok) {
+          return response.status(res.status).json(json);
+        }
+
+        return json;
+      })
+      .catch((e) => {
+        return response.status(500).json(e);
+      });
+
+    response.json({
+      email: user.login,
+    });
   }
-
-  res.json({
-    email: "some@somes.com",
-  });
 };
 
 export default UserHandler;
