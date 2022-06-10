bf756104bec609709f65388b7addb083535f3ed5
Marcus-Rise <ilykonst95@gmail.com>
2022-06-10 09:32:23 +0400

    refactor(api): logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,5 +1,6 @@
 import type { NextApiHandler } from "next";
 import { removeCookie } from "../../../src/server/cookie";
+import { withMethodHandlers } from "../../../src/server/utils/interceptor";
 
 const LogoutHandler: NextApiHandler = (req, res) => {
   removeCookie(res, "auth");
@@ -7,4 +8,4 @@ const LogoutHandler: NextApiHandler = (req, res) => {
   res.status(200).end();
 };
 
-export default LogoutHandler;
+export default withMethodHandlers({ method: "GET", handler: LogoutHandler })();
