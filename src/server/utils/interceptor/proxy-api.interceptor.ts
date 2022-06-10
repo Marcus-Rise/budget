69202adfa944ffaf978aba9306d4afaa1ae45625
Marcus-Rise <ilykonst95@gmail.com>
2022-06-10 11:35:07 +0400

    feat(api): auth prolongation

--- src/server/utils/interceptor/proxy-api.interceptor.ts
+++ src/server/utils/interceptor/proxy-api.interceptor.ts
@@ -1,8 +1,9 @@
 import type { Interceptor } from "./interceptor";
 import httpProxyMiddleware from "next-http-proxy-middleware";
+import { removeCookie } from "../../cookie";
 
-const withProxyApi: Interceptor = () => (req, res) =>
-  httpProxyMiddleware(req, res, {
+const withProxyApi: Interceptor = () => (req, response) =>
+  httpProxyMiddleware(req, response, {
     target: process.env.API_URL,
     pathRewrite: [
       {
@@ -10,6 +11,18 @@ const withProxyApi: Interceptor = () => (req, res) =>
         replaceStr: "/api",
       },
     ],
+    onProxyInit: (proxy) => {
+      /**
+       * Check the list of bindable events in the `http-proxy` specification.
+       * @see https://www.npmjs.com/package/http-proxy#listening-for-proxy-events
+       */
+      // proxy.on("proxyReq", (proxyReq, req, res) => {});
+      proxy.on("proxyRes", (proxyRes, req, res) => {
+        if (proxyRes.statusCode === 401) {
+          removeCookie(res, "auth");
+        }
+      });
+    },
   });
 
 export { withProxyApi };
