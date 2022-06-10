ad11e5edf1784f73b0c3b6e6bd22af2bb9b2bf12
Marcus-Rise <ilykonst95@gmail.com>
2022-06-10 13:49:43 +0400

    feat(api): auth logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,11 +1,42 @@
 import type { NextApiHandler } from "next";
 import { removeCookie } from "../../../src/server/cookie";
-import { withMethodHandlers } from "../../../src/server/utils/interceptor";
+import {
+  applyInterceptors,
+  withAuth,
+  withMethodHandlers,
+} from "../../../src/server/utils/interceptor";
+import type { LoginResponseDto } from "../../../src/server/dto";
 
-const LogoutHandler: NextApiHandler = (req, res) => {
-  removeCookie(res, "auth");
+const LogoutHandler: NextApiHandler = (req, response) => {
+  const { auth: stringifyDto } = req.cookies;
+  const { access_token, type, refresh_token }: LoginResponseDto = JSON.parse(stringifyDto);
 
-  res.status(200).end();
+  return fetch(process.env.API_URL + "/api/auth/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${type} ${access_token}`,
+    },
+    body: JSON.stringify({ refresh_token }),
+  })
+    .then(async (res) => {
+      const json = await res.json();
+
+      if (!res.ok) {
+        return response.status(res.status).json(json);
+      }
+
+      return json;
+    })
+    .then(() => {
+      removeCookie(response, "auth");
+
+      response.status(200).end();
+    });
 };
 
-export default withMethodHandlers({ method: "GET", handler: LogoutHandler })();
+export default applyInterceptors(
+  () => {},
+  withMethodHandlers({ method: "GET", handler: LogoutHandler }),
+  withAuth,
+);
