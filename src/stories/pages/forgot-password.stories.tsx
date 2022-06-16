f13770641d5b1e66bf3eac588868ec06d3f60b10
Ilya Konstantinov <ilykonst95@gmail.com>
2022-06-16 12:09:45 +0400

    feat: storybook msw (#35)

--- src/stories/pages/forgot-password.stories.tsx
+++ src/stories/pages/forgot-password.stories.tsx
@@ -1,5 +1,6 @@
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import ForgotPassword from "../../../pages/forgot-password/index";
+import { rest } from "msw";
 
 const Config: ComponentMeta<typeof ForgotPassword> = {
   title: "pages/ForgotPassword",
@@ -9,6 +10,26 @@ const Config: ComponentMeta<typeof ForgotPassword> = {
 const Template: ComponentStory<typeof ForgotPassword> = (args) => <ForgotPassword {...args} />;
 
 const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.post("/api/auth/proxy/reset-password", (req, res, ctx) => {
+        return res(ctx.status(200));
+      }),
+    ],
+  },
+};
+
+const Failed = Template.bind({});
+Failed.parameters = {
+  msw: {
+    handlers: [
+      rest.post("/api/auth/proxy/reset-password", (req, res, ctx) => {
+        return res(ctx.status(500));
+      }),
+    ],
+  },
+};
 
 export default Config;
-export { Default };
+export { Default, Failed };
