a9727b56a2c3078254d202101bd73af0edcd0ba0
Marcus-Rise <ilykonst95@gmail.com>
2022-06-22 16:33:31 +0400

    fix(auth): password length validator

--- src/auth/helpers.ts
+++ src/auth/helpers.ts
@@ -2,6 +2,6 @@ const PASSWORD_MIN_LENGTH = 8;
 
 const passwordLengthValidator = (value: string): boolean | string =>
   value.length >= PASSWORD_MIN_LENGTH ||
-  `Длинна логина должна быть не меньше ${PASSWORD_MIN_LENGTH}`;
+  `Длинна пароля должна быть не меньше ${PASSWORD_MIN_LENGTH} символов`;
 
 export { passwordLengthValidator };
