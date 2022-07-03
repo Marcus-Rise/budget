ef4f2bae7094f573b1ba7453d8c4c60ef302342e
Ilya Konstantinov <ilykonst95@gmail.com>
2022-07-03 13:02:40 +0400

    Feat: change password in profile (#58)

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,6 +7,11 @@ import { Button, ButtonVariant } from "../src/components/button";
 import { useRouter } from "next/router";
 import styled, { useTheme } from "styled-components";
 import { useAuth } from "../src/auth";
+import type { ChangePasswordFormProps } from "../src/auth/components/change-password-form";
+import { ChangePasswordForm } from "../src/auth/components/change-password-form";
+import { useState } from "react";
+import { PopupType, usePopup } from "../src/components/popup";
+import { FormProvider, useForm } from "react-hook-form";
 
 const ProfileContainer = styled(Container)`
   padding-top: 1rem;
@@ -14,8 +19,22 @@ const ProfileContainer = styled(Container)`
   align-items: center;
 `;
 
+const ProfileCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
 const Title = styled.h2`
   font-size: 1.25rem;
+`;
+
+const ChangePasswordFormTitle = styled.h3`
+  font-size: 1rem;
+`;
+
+const ChangePasswordFormWrapper = styled.div`
+  max-width: 20rem;
 `;
 
 const ActionsContainer = styled.div`
@@ -28,6 +47,9 @@ const Profile: NextPage = () => {
   const auth = useAuth();
   const router = useRouter();
   const theme = useTheme();
+  const [changePasswordLoading, setChangePasswordLoading] = useState(false);
+  const popup = usePopup();
+  const changePasswordMethods = useForm();
 
   const logout = async () => {
     await auth.logout();
@@ -49,18 +71,36 @@ const Profile: NextPage = () => {
     }
   };
 
+  const changePassword: ChangePasswordFormProps["onSubmit"] = (dto) => {
+    setChangePasswordLoading(true);
+
+    return auth
+      .changePassword(dto)
+      .then(() => {
+        popup.open("Ваш пароль изменен!", PopupType.SUCCESS);
+
+        changePasswordMethods.reset();
+      })
+      .catch(() => popup.open("Не удалось изменить пароль", PopupType.DANGER))
+      .finally(() => setChangePasswordLoading(false));
+  };
+
   return (
     <LayoutPrivate>
       <ProfileContainer>
-        <Card>
+        <ProfileCard>
           <Title>Профиль</Title>
           {!user || isLoading ? (
             "Loading..."
           ) : (
             <>
-              Login: {user.login}
-              <br />
-              <br />
+              Логин: {user.login}
+              <ChangePasswordFormWrapper>
+                <ChangePasswordFormTitle>Смена пароля</ChangePasswordFormTitle>
+                <FormProvider {...changePasswordMethods}>
+                  <ChangePasswordForm onSubmit={changePassword} loading={changePasswordLoading} />
+                </FormProvider>
+              </ChangePasswordFormWrapper>
               <ActionsContainer>
                 <Button onClick={logout}>Выйти</Button>
                 <Button variant={ButtonVariant.TEXT} color={theme.danger} onClick={deleteAccount}>
@@ -69,7 +109,7 @@ const Profile: NextPage = () => {
               </ActionsContainer>
             </>
           )}
-        </Card>
+        </ProfileCard>
       </ProfileContainer>
     </LayoutPrivate>
   );
--- src/stories/pages/profile.stories.tsx
+++ src/stories/pages/profile.stories.tsx
@@ -1,19 +1,47 @@
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import Profile from "../../../pages/profile";
+import type { IUser } from "../../user";
 import { UserProvider } from "../../user";
+import { rest } from "msw";
 
 const Config: ComponentMeta<typeof Profile> = {
   title: "pages/Profile",
   component: Profile,
 };
 
-const Default: ComponentStory<typeof Profile> = (args) => <Profile {...args} />;
-
-const WithUser: ComponentStory<typeof Profile> = (args) => (
-  <UserProvider user={{ login: "some@some.com" }}>
+const Template: ComponentStory<typeof Profile> = (args) => (
+  <UserProvider>
     <Profile {...args} />
   </UserProvider>
 );
 
+const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get<IUser>("/api/proxy/user", (req, res, ctx) => {
+        return res(ctx.json({ login: "some@some.com" }));
+      }),
+      rest.post("/api/auth/change-password", (req, res, ctx) => {
+        return res(ctx.status(200));
+      }),
+    ],
+  },
+};
+
+const FailedChangePassword = Template.bind({});
+FailedChangePassword.parameters = {
+  msw: {
+    handlers: [
+      rest.get<IUser>("/api/proxy/user", (req, res, ctx) => {
+        return res(ctx.json({ login: "some@some.com" }));
+      }),
+      rest.post("/api/auth/change-password", (req, res, ctx) => {
+        return res(ctx.status(500));
+      }),
+    ],
+  },
+};
+
 export default Config;
-export { Default, WithUser };
+export { Default, FailedChangePassword };
